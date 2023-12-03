64545c33050f33c437f5affffe41bfdd27f50784
Salvador Montiel <salvoperez51@gmail.com>
2023-12-03 14:05:53 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from functions import get_todos, write_todos
+import time
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
+    if user_action.startswith('add'):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + '\n')
+
+        write_todos(todos)
+
+    elif user_action.startswith('show'):
+        todos = get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            print(f"{index + 1}. {item}")
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+
+            todos = get_todos()
+
+            todos[number - 1] = input("Enter new todo: ") + '\n'
+
+            write_todos(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
+
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+            todos = get_todos()
+
+            index = number - 1
+            removed_todo = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos(todos)
+
+            message = f"To-Do '{removed_todo}' was removed from the list"
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("Command is not valid.")
+print("Bye!")
